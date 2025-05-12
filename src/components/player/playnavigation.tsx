1b1821ed061b61aa6b96773e277ee8ea0d697240
safadigital <safarov@safadigital.su>
2025-05-12 22:33:49 +0500

    added back icon for player

--- src/components/player/playnavigation.tsx
+++ src/components/player/playnavigation.tsx
@@ -157,6 +157,13 @@ const percentage_video = Math.floor(((currentFrame / fps) / (duration / 1000)) *
 <div className='w-[100%] h-[100%] top-[0%] fixed bg-black bg-opacity-70'>
 <div className='ml-5 mr-5'>
 
+  <div className='visible sm:invisible top-[2%] fixed flex w-[40px] h-[40px] bg-[#141D19] items-center justify-center rounded-full cursor-pointer'>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+  <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
+</svg>
+
+  </div>
+
   <div className='visible sm:invisible bottom-[35%] fixed flex w-full text-[#FFF] font-bold text-[18px] new_york_medium_font'>
     Meditation, breath-work, and mindfulness exercises
   </div>
