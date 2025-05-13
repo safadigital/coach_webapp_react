fdc581f04883f9e3e9af71acd17206f267a0087c
safadigital <safarov@safadigital.su>
2025-05-13 13:51:54 +0500

    fixed styles of icons in player

--- src/components/player/playnavigation.tsx
+++ src/components/player/playnavigation.tsx
@@ -164,7 +164,7 @@ const percentage_video = Math.floor(((currentFrame / fps) / (duration / 1000)) *
 <div className='ml-5 mr-5'>
 
   <div className='visible sm:invisible top-[2%] fixed flex w-[40px] h-[40px] bg-[#141D19] items-center justify-center rounded-full cursor-pointer'>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4 text-[#fff] font-bold">
   <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
 </svg>
   </div>
@@ -213,7 +213,7 @@ const percentage_video = Math.floor(((currentFrame / fps) / (duration / 1000)) *
 <div className={"visible sm:invisible bottom-7 fixed flex text-center w-full pl-[27%]"}>
 
      
-      <span className='visible sm:invisible bottom-11 left-8 mr-20 fixed flex w-[32px] h-[32px] bg-[#141D19] items-center justify-center rounded-full cursor-pointer'>
+      <span className='visible sm:invisible bottom-11 left-8 mr-20 fixed flex w-[32px] h-[32px] bg-[#141D19] text-[#fff] font-bold items-center justify-center rounded-full cursor-pointer'>
     {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
   <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
 </svg> */}
@@ -312,7 +312,7 @@ xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={
         {
           isAudioMuted ? (
 <span onClick={unmuteSound} className={"cursor-pointer flex items-center pl-8"}>
-       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-[#fff]">
   <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM17.78 9.22a.75.75 0 1 0-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 1 0 1.06-1.06L20.56 12l1.72-1.72a.75.75 0 1 0-1.06-1.06l-1.72 1.72-1.72-1.72Z" />
 </svg>
 
@@ -320,7 +320,7 @@ xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={
         </span>
           ) : (
 <span onClick={muteSound} className={"cursor-pointer flex items-center pl-8"}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 text-[#fff]">
   <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 0 0 1.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06ZM18.584 5.106a.75.75 0 0 1 1.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 0 1-1.06-1.06 8.25 8.25 0 0 0 0-11.668.75.75 0 0 1 0-1.06Z" />
   <path d="M15.932 7.757a.75.75 0 0 1 1.061 0 6 6 0 0 1 0 8.486.75.75 0 0 1-1.06-1.061 4.5 4.5 0 0 0 0-6.364.75.75 0 0 1 0-1.06Z" />
 </svg>
