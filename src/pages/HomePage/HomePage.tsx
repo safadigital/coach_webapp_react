476f9cb00dd6e13faa483f3ec8b57954afd96bfd
safadigital <safarov@safadigital.su>
2025-05-13 16:07:49 +0500

    added lessons

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,7 @@ import leftArrov from '../../assets/left_arrow.svg';
 import rightArrov from '../../assets/right_arrow.svg';
 
 import statusDone from '../../assets/status_done.svg';
+import statusNext from '../../assets/status_next.svg';
 
 // fake data
 import data from '../../mock_data/daily_plan.json';
@@ -13,8 +14,10 @@ import data from '../../mock_data/daily_plan.json';
 const HomePage = () => {
 
 
-    console.log(data.plan[0].total_days);
+    console.log(data.plan[0].questions);
 
+    const headline = data.plan[0].headline;
+    const lessons = data.plan[0].questions;
     const total_days = data.plan[0].total_days;
     const day_in_program = data.plan[0].day_in_program;
 
@@ -25,7 +28,7 @@ const HomePage = () => {
         <>
          <header className="visible sm:invisible lg:invisible flex items-center justify-between pr-5 pl-5 pt-5">
         <div className="flex-column">
-            <h3 className="text-xl font-bold new_york_medium_font">Desire & Pleasure</h3>
+            <h3 className="text-xl font-bold new_york_medium_font">{headline}</h3>
             <div className="flex items-start pt-3">
                 <div className="w-20 bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-200">
                     <div className={`bg-[#FF6D03] h-1.5 rounded-full dark:bg-[#FF6D03] ${class_progress}`}>
@@ -58,6 +61,30 @@ const HomePage = () => {
 <p className="text-[#696E6C] text-[10px] uppercase font-bold sp_pro_text_medium_font wide">Daily lessons</p>
     </div>
 
+
+{
+
+lessons.map((lesson, idx) => (
+  <div key={idx} className="visible sm:invisible columns pr-5 pl-5 pt-1 mt-2 mb-2 ">
+  
+    <div className="flex bg-[#F3F4F4] topleftrounded bottomleftrounded pt-[16px] pl-[16px] pb-[16px] bottom_shadow">
+        <img className="couseimage" src={lesson.main_image} alt="" />
+    </div>
+    <div className="flex-column bottomrightrounded toprightrounded content-between bg-[#F3F4F4] pl-3 pr-[16px] pt-[16px] pb-[16px] bottom_shadow">
+        <div className="flex flex-row w-full justify-between items-center">
+            <p className="text-[16px] font-bold new_york_medium_font mr-1">{lesson.headline}</p>
+            <img className="w-[30px] h-[30px]" src={ lesson.completed ? statusDone : statusNext} alt="" />
+                </div>
+                <div className="mttauto">
+                    <p className="text-[10px] text-[#696E6C] uppercase font-bold sp_pro_text_medium_font wide">{lesson.description}</p>
+                    <p className="text-[10px] text-[#696E6C] uppercase font-bold sp_pro_text_medium_font wide">{lesson.duration}</p>     
+                        </div>
+    </div>
+   </div>
+))
+
+}
+
     {/* <!-- card --> */}
    <div className="visible sm:invisible columns pr-5 pl-5 pt-1 mt-2 mb-2 ">
   
@@ -75,6 +102,8 @@ const HomePage = () => {
                         </div>
     </div>
    </div>
+
+
    {/* <!-- end of card -->
 
     <!-- card --> */}
@@ -86,7 +115,7 @@ const HomePage = () => {
         <div className="flex-column bottomrightrounded toprightrounded content-between bg-[#F3F4F4] pl-3 pr-[16px] pt-[16px] pb-[16px] bottom_shadow">
             <div className="flex flex-row w-full justify-between items-center">
                 <p className="text-[16px] font-bold new_york_medium_font mr-1">Getting to know your body</p>
-                <img className="w-[30px] h-[30px]" src="images/status_next.svg" alt="" />
+                <img className="w-[30px] h-[30px]" src={statusNext} alt="" />
                     </div>
                     <div className="mttauto">
                         <p className="text-[10px] text-[#696E6C] uppercase font-bold sp_pro_text_medium_font wide">Lesson 2</p>
